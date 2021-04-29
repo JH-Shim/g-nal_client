5e262fc3d5b793c968cc094a60ba2a0d7bb90467
심종훈 <sayhello.shim@gmail.com>
2021-04-29 18:55:52 +0900

    [CLIENT][ADD]네이버 지도 API 적용

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-g-nal_client
+# g-nal_client
--- src/components/index/Map.js
+++ src/components/index/Map.js
@@ -1,13 +1,30 @@
 import React from 'react';
 // import { useHistory } from 'react-router-dom';
-// import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps';
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps';
 // import Geolocation from './Geolocation';
-// import EasterEgg from './EasterEgg';
 
 function Map({}) {
   // const history = useHistory();
 
-  return <div className="flex70">Map</div>;
+  return (
+    <div className="flex70">
+      <RenderAfterNavermapsLoaded
+        ncpClientId={process.env.REACT_APP_NAVER_MAP_NCPCLIENTID}
+        error={<p>Maps Load Error</p>}
+        loading={<p>Maps Loading...</p>}
+      >
+        <NaverMap
+          // mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+          defaultCenter={{ lat: 37.486592, lng: 126.943232 }}
+          defaultZoom={15}
+        />
+      </RenderAfterNavermapsLoaded>
+    </div>
+  );
 }
 
 export default Map;
