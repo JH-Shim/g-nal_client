291e1e7fb343b3b894bdecb32efbc2a4f623135e
심종훈 <sayhello.shim@gmail.com>
2021-05-03 00:16:30 +0900

    [CLIENT][FIX]SignUp, SignIn CSS
    완료

--- src/components/accounts/SignIn.js
+++ src/components/accounts/SignIn.js
@@ -6,8 +6,25 @@ function SignIn({}) {
 
   return (
     <div className="rem35 container_grid center_grid bg_contrast">
-      <div className="center_div_res_container container_flex_column">
-        <div className="signin_up flex80"></div>
+      <div className="container_flex_column center_div_res_container">
+        <div className="signin_up flex80 container_flex_column">
+          <div className="flex10"></div>
+          <div className="flex10 container_padding">
+            <input className="hw100" placeholder="아이디"></input>
+          </div>
+          <div className="flex10 container_padding">
+            <input
+              className="hw100"
+              placeholder="비밀번호"
+              type="password"
+            ></input>
+          </div>
+          <div className="flex10 container_padding">
+            <div className="hw100 bg_eee container_grid center_grid pointer">
+              로그인
+            </div>
+          </div>
+        </div>
         <div
           className="signin_down flex20 container_grid center_grid"
           onClick={() => {
