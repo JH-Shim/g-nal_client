8caa00c62a232511a3f3053ffae5a36a048a7f83
심종훈 <sayhello.shim@gmail.com>
2021-05-07 21:28:08 +0900

    [CLIENT][ADD]Geolocation

--- src/components/index/Map.js
+++ src/components/index/Map.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { useHistory } from 'react-router-dom';
 import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps';
-// import Geolocation from './Geolocation';
+import Geolocation from './Geolocation';
 
 function Map({}) {
+  const [geolocation, setGeolocation] = useState({ lat: 0, lng: 0 });
+  const [places, setPlaces] = useState(null);
+
   // const history = useHistory();
 
   return (
@@ -13,15 +16,40 @@ function Map({}) {
         error={<p>Maps Load Error</p>}
         loading={<p>Maps Loading...</p>}
       >
+        <Geolocation setGeolocation={setGeolocation} />
         <NaverMap
-          // mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+          // mapDivId={'naver_map'} // default: react-naver-map
           style={{
             width: '100%',
             height: '100%',
           }}
-          defaultCenter={{ lat: 37.486592, lng: 126.943232 }}
           defaultZoom={15}
-        />
+          center={geolocation}
+          // defaultCenter={geolocation} // ! check center와의 차이
+          onClick={(e) => {
+            // ! console.log(e);
+            // ! console.log(e.coord);
+            setGeolocation({ lat: e.coord._lat, lng: e.coord._lng });
+          }}
+        >
+          <Marker position={geolocation} animation={1} onClick={() => {}} />
+          {!places ? (
+            <div className="display_none"></div>
+          ) : (
+            <div>
+              {places.map((place) => (
+                <Marker
+                  key={place.id}
+                  position={{
+                    lat: place.lat,
+                    lng: place.lng,
+                  }}
+                  // onClick={() => {}} // ! check onClick. 위에 Marker onClick도 신경쓰길
+                />
+              ))}
+            </div>
+          )}
+        </NaverMap>
       </RenderAfterNavermapsLoaded>
     </div>
   );
