64cb245a3251f2b55b60c1719ad6692d67edfd1c
심종훈 <sayhello.shim@gmail.com>
2021-05-11 21:04:10 +0900

    [CLIENT][FIX]maintenance

--- src/components/index/PlaceRegister.js
+++ src/components/index/PlaceRegister.js
@@ -21,16 +21,15 @@ class PlaceRegister extends React.Component {
     let reader = new FileReader();
 
     reader.onloadend = () => {
-      // 2. 읽기가 완료되면 아래코드가 실행됩니다.
       const base64 = reader.result;
       if (base64) {
-        this.setState({ imgBase64: base64.toString() }); // 파일 base64 상태 업데이트
+        this.setState({ imgBase64: base64.toString() });
       }
     };
 
     if (e.target.files[0]) {
-      reader.readAsDataURL(e.target.files[0]); // 1. 파일을 읽어 버퍼에 저장합니다.
-      this.setState({ file: e.target.files[0] }); // 파일 상태 업데이트
+      reader.readAsDataURL(e.target.files[0]);
+      this.setState({ file: e.target.files[0] });
     }
   };
 
